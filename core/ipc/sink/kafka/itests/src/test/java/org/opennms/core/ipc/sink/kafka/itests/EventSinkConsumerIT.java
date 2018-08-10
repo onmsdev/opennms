7ba9f68624a194b83991c1d019812a28f610a225
Malatesh <Malatesh.Sudarshan@cerner.com>
2018-08-11 00:30:00 +0530

    Adding opennms copyright header

--- core/ipc/sink/kafka/itests/src/test/java/org/opennms/core/ipc/sink/kafka/itests/EventSinkConsumerIT.java
+++ core/ipc/sink/kafka/itests/src/test/java/org/opennms/core/ipc/sink/kafka/itests/EventSinkConsumerIT.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 /*******************************************************************************
  * This file is part of OpenNMS(R).
  *
@@ -29,34 +27,19 @@
  *******************************************************************************/
 
 
->>>>>>> mala/develop_eventConsumer
 package org.opennms.core.ipc.sink.kafka.itests;
 
 import static com.jayway.awaitility.Awaitility.await;
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static org.hamcrest.Matchers.equalTo;
-<<<<<<< HEAD
-import static org.junit.Assert.assertEquals;
-=======
->>>>>>> mala/develop_eventConsumer
 import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.Hashtable;
-<<<<<<< HEAD
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.Before;
-import org.junit.Ignore;
-=======
-import java.util.concurrent.atomic.AtomicInteger;
-
-import org.junit.Before;
->>>>>>> mala/develop_eventConsumer
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -64,10 +47,6 @@ import org.opennms.core.ipc.sink.api.MessageConsumer;
 import org.opennms.core.ipc.sink.api.MessageDispatcherFactory;
 import org.opennms.core.ipc.sink.api.SinkModule;
 import org.opennms.core.ipc.sink.api.SyncDispatcher;
-<<<<<<< HEAD
-import org.opennms.core.ipc.sink.common.ThreadLockingMessageConsumer;
-=======
->>>>>>> mala/develop_eventConsumer
 import org.opennms.core.ipc.sink.kafka.client.KafkaRemoteMessageDispatcherFactory;
 import org.opennms.core.ipc.sink.kafka.common.KafkaSinkConstants;
 import org.opennms.core.ipc.sink.kafka.server.KafkaMessageConsumerManager;
@@ -82,15 +61,6 @@ import org.osgi.service.cm.ConfigurationAdmin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 
-<<<<<<< HEAD
-import com.codahale.metrics.Meter;
-import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.Timer;
-import com.codahale.metrics.Timer.Context;
-import com.google.common.util.concurrent.RateLimiter;
-
-=======
->>>>>>> mala/develop_eventConsumer
 @RunWith(OpenNMSJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
         "classpath:/META-INF/opennms/applicationContext-soa.xml",
@@ -173,99 +143,4 @@ public class EventSinkConsumerIT {
 
     }
 
-<<<<<<< HEAD
-    @Test(timeout = 60000)
-    @Ignore("flapping")
-    public void canConsumeMessagesInParallel() throws Exception {
-        final int NUM_CONSUMER_THREADS = 7;
-
-        final EventsModule parallelEventsModule = new EventsModule(m_config) {
-            @Override
-            public int getNumConsumerThreads() {
-                return NUM_CONSUMER_THREADS;
-            }
-        };
-
-        final ThreadLockingMessageConsumer<Event, Log> consumer = new ThreadLockingMessageConsumer<>(parallelEventsModule);
-
-        final CompletableFuture<Integer> future = consumer.waitForThreads(NUM_CONSUMER_THREADS);
-
-        try {
-            consumerManager.registerConsumer(consumer);
-
-            final SyncDispatcher<Event> dispatcher = remoteMessageDispatcherFactory.createSyncDispatcher(new EventsModule(m_config));
-
-            final EventGenerator generator = new EventGenerator(dispatcher,
-                                                                        100.0);
-            generator.start();
-
-            // Wait until we have NUM_CONSUMER_THREADS locked
-            future.get();
-
-            // Take a snooze
-            Thread.sleep(TimeUnit.SECONDS.toMillis(5));
-
-            // Verify that there aren't more than NUM_CONSUMER_THREADS waiting
-            assertEquals(0, consumer.getNumExtraThreadsWaiting());
-
-            generator.stop();
-        } finally {
-            consumerManager.unregisterConsumer(consumer);
-        }
-    }
-
-    public static class EventGenerator {
-        Thread thread;
-
-        final SyncDispatcher<Event> dispatcher;
-        final double rate;
-        final AtomicBoolean stopped = new AtomicBoolean(false);
-        private final Meter sentMeter;
-        private final Timer sendTimer;
-
-        public EventGenerator(SyncDispatcher<Event> dispatcher, double rate) {
-            this.dispatcher = dispatcher;
-            this.rate = rate;
-            MetricRegistry metrics = new MetricRegistry();
-            this.sentMeter = metrics.meter("sent");
-            this.sendTimer = metrics.timer("send");
-        }
-
-        public EventGenerator(SyncDispatcher<Event> dispatcher, double rate,
-                Meter sentMeter, Timer sendTimer) {
-            this.dispatcher = dispatcher;
-            this.rate = rate;
-            this.sentMeter = sentMeter;
-            this.sendTimer = sendTimer;
-        }
-
-        public synchronized void start() {
-            stopped.set(false);
-            final RateLimiter rateLimiter = RateLimiter.create(rate);
-            thread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-
-                    while (!stopped.get()) {
-                        rateLimiter.acquire();
-                        try (Context ctx = sendTimer.time()) {
-                            dispatcher.send(new Event());
-                            sentMeter.mark();
-                        }
-                    }
-                }
-            });
-            thread.start();
-        }
-
-        public synchronized void stop() throws InterruptedException {
-            stopped.set(true);
-            if (thread != null) {
-                thread.join();
-                thread = null;
-            }
-        }
-    }
-=======
->>>>>>> mala/develop_eventConsumer
 }
